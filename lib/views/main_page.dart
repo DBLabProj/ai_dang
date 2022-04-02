dd3d38f5c7b5fafac4977f0e9b18888b9f6e984d
yso009 <yso009@naver.com>
2022-04-02 13:38:15 +0900

    add image URL

---
+++ lib/views/main_page.dart
@@ -0,0 +1,13 @@
+class main_page extends StatefulWidget {
+  const main_page({Key? key}) : super(key: key);
+
+  @override
+  _main_pageState createState() => _main_pageState();
+}
+
+class _main_pageState extends State<main_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
