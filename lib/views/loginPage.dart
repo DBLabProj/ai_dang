123ae4dea57d80a4f0af6ebbb8f3b0eec99f1c0b
yso009 <yso009@naver.com>
2022-04-16 18:40:21 +0900

    feat : add GestureDetector

---
+++ lib/views/loginPage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class loginPage extends StatelessWidget {
+  const loginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+      //FocusManager.instance.primaryFocus?.unfocus();
+      FocusScope.of(context).unfocus();
+    },
+    child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: (MediaQuery.of(context).size.height) * 0.15,
+              ),
+              Container(
+                color: Colors.white,
+                height: (MediaQuery.of(context).size.height) -
+                    (MediaQuery.of(context).size.height) * 0.3,
+                width: (MediaQuery.of(context).size.width) -
+                    (MediaQuery.of(context).size.width) * 0.3,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'AI Dang',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: (MediaQuery.of(context).size.width) * 0.16,
+                          color: Color(0xffCF2525)),
+                    ),
+                    Text(
+                      '나만의 스마트한 당뇨관리 비서',
+                      style: TextStyle(
+                          fontSize:
+                          ((MediaQuery.of(context).size.width) * 0.16) *
+                              0.26,
+                          color: Color(0xffCF2525)),
+                    ),
+                    SizedBox(
+                      height: (MediaQuery.of(context).size.height) * 0.15,
+                    ),
+
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        labelText: 'Username',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),),);
+  }
+}
