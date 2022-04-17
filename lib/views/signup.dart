4231bc3219d4a57a63af00b027773207d5dbd44b
yso009 <yso009@naver.com>
2022-04-17 18:12:28 +0900

    feat : :

---
+++ lib/views/signup.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class signup extends StatelessWidget {
+  const signup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        //FocusManager.instance.primaryFocus?.unfocus();
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: (MediaQuery.of(context).size.height) * 0.15,
+              ),
+              Container(
+                color: Colors.white,
+                height: (MediaQuery.of(context).size.height) -
+                    (MediaQuery.of(context).size.height) * 0.2,
+                width: (MediaQuery.of(context).size.width) -
+                    (MediaQuery.of(context).size.width) * 0.3,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '정보를 입력해 주세요',
+                      style: TextStyle(
+                          fontSize:
+                          ((MediaQuery.of(context).size.width) * 0.16) *
+                              0.26,
+                          color: Color(0xffCF2525)),
+                    ),
+                    SizedBox(height: (MediaQuery.of(context).size.height) * 0.10,
+                    ),
+                    SizedBox(
+                      width: (MediaQuery.of(context).size.width) -
+                          (MediaQuery.of(context).size.width) * 0.35,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter your email',
+                          labelStyle: TextStyle(color: Color(0xffCF2525)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+
+                      ),
+                    ),
+                    SizedBox(height: (MediaQuery.of(context).size.height) * 0.05,
+                    ),
+
+                    SizedBox(
+                      width: (MediaQuery.of(context).size.width) -
+                          (MediaQuery.of(context).size.width) * 0.35,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter your email',
+                          labelStyle: TextStyle(color: Color(0xffCF2525)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+
+                      ),
+                    ),
+                    SizedBox(height: (MediaQuery.of(context).size.height) * 0.05,
+                    ),
+                    SizedBox(
+                      width: (MediaQuery.of(context).size.width) -
+                          (MediaQuery.of(context).size.width) * 0.35,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          // filled: true,
+                          labelText: 'Password',
+                          hintText: 'Enter your password',
+                          labelStyle: TextStyle(color: Color(0xffCF2525)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),),);
+  }
+}
