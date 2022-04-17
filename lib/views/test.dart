7c892cf868d0ece8dd30712ac86aeca4ad8c4b7e
yso009 <yso009@naver.com>
2022-04-17 16:47:29 +0900

    test : test 지금 시작하기 button

---
+++ lib/views/test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class testpage extends StatelessWidget {
+  const testpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'testpage입니다.'
+        ),
+      ),
+    );
+  }
+}
