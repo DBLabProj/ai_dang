56689943661c991744b2863f2cf190cb2b122161
yso009 <yso009@naver.com>
2022-05-22 17:09:22 +0900

    test : add bottomnavigationbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:ai_dang/views/navbartest.dart';
+import 'package:ai_dang/views/test.dart';
 import 'package:flutter/material.dart';
 import 'dbHandler.dart';
 import 'views/camera.dart';
 import 'views/loginPage.dart';
 import 'views/home.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:ai_dang/views/setting/setting.dart';
 
 void main() {
   initializeDateFormatting().then((_) => runApp(MyApp()));
@@ -15,19 +18,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: '메인페이지 AppBar'),
+      title: 'First App',
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -37,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+
     var db = DbHandler();
     db.connect().then((conn) {
       db.printData(conn).then((results) {
@@ -48,59 +48,134 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
     });
+
   }
+
+  int selectedindex=0;
+  List _pages=[MyStatefulWidget(),loginPage(),Text('page3'), setting()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '첫 화면 메인 페이지11',
-            ),TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.blue,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CameraExample())
-                );
-              },
-              child: Text('카메라 페이지로 이동'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.blue,
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => loginPage())
-                );
-              },
-              child: Text('로그인 페이지로 이동'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.blue,
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage())
-                );
-              },
-              child: Text('홈 페이지로 이동'),
-            )
-          ],
-        ),
+        child: _pages[selectedindex],
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         '첫 화면 메인 페이지11',
+      //       ),
+      //
+      //       TextButton(
+      //         style: TextButton.styleFrom(
+      //           primary: Colors.blue,
+      //         ),
+      //         onPressed: () {
+      //           // print(_incrementCounter);
+      //           Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => navbartest())
+      //           );
+      //         },
+      //         child: Text('테스트 페이지로 이동'),
+      //       ),
+      //       TextButton(
+      //         style: TextButton.styleFrom(
+      //           primary: Colors.blue,
+      //         ),
+      //         onPressed: () {
+      //           Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => loginPage())
+      //           );
+      //         },
+      //         child: Text('로그인 페이지로 이동'),
+      //       ),
+      //       TextButton(
+      //         style: TextButton.styleFrom(
+      //           primary: Colors.blue,
+      //         ),
+      //         onPressed: () {
+      //           Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => HomePage())
+      //           );
+      //         },
+      //         child: Text('홈 페이지로 이동'),
+      //       )
+      //     ],
+      //   ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Color(0xffCF2525),
+        type: BottomNavigationBarType.fixed,
+
+        onTap: _onItemTapped,
+        currentIndex: selectedindex,
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Statistics',
+            icon: Icon(Icons.insert_chart),
+          ),
+          BottomNavigationBarItem(
+            label: 'My Page',
+            icon: Icon(Icons.person),
+          ),
+          BottomNavigationBarItem(
+            label: 'Setting',
+            icon: Icon(Icons.settings ),
+          ),
+        ],
       ),
     );
+  }
+  void _onItemTapped(int index){
+    setState(() {
+      selectedindex=index;
+      // _switchScreen(selectedindex);
+    });
+  }
+  void _switchScreen(index){
+    switch (index) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MyStatefulWidget()
+          ),
+        );
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => testpage()
+          ),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => testpage()
+          ),
+        );
+        break;
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => setting()
+          ),
+        );
+        break;
+
+    }
   }
 }
