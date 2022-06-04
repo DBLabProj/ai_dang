4d7707b2a873ed2f4842b04e794c3639e5ddbf1f
Heebeom Yang <takeny1998@gmail.com>
2022-06-04 20:42:15 +0900

    fix: meal showing error if it's first time to show
    1. 처음 진입 시 식단이 보이지 않는 문제 해결
     - _selectedDay에 DateTime객체를 UTC 객체로 바꿔줌
     - UTC 시간 동기화 위해 9시간 더해줌
    
    2. 측정결과 UI 수정
     - 텍스트와 아이콘 사이즈 축소
     - 인디게이터바 추가

--- lib/views/mealListBuilder.dart
+++ lib/views/mealListBuilder.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import '../dbHandler.dart';
 
