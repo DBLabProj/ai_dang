962a5c9c1ac48a46f4ec7a02ae4a6e35cfbf9b9b
Heebeom Yang <takeny1998@gmail.com>
2022-06-06 21:41:34 +0900

    feat: loginpage style edit
    1. 로그인페이지 스타일 변경

--- lib/views/loginPage.dart
+++ lib/views/loginPage.dart
@@ -8,6 +8,10 @@ import 'package:ai_dang/dbHandler.dart';
 import '../main.dart';
 import 'home.dart';
 
+Color red = const Color(0xffCF2525);
+Color gray = const Color(0xffD6D6D6);
+Color darKGray = const Color(0xff3E3E3E);
+
 class loginPage extends StatefulWidget {
   const loginPage({Key? key}) : super(key: key);
 
@@ -20,7 +24,9 @@ class _loginPageState extends State<loginPage> {
   final _passwordTextEditController = TextEditingController();
 
   bool Login_check = false;
-
+  List<FocusNode> _focusNodes = [
+    FocusNode(), FocusNode()
+  ];
   void user_check(Email, Password) async {
     var conn = await ConnHandler.instance.conn;
 
@@ -42,18 +48,18 @@ class _loginPageState extends State<loginPage> {
 
     var User_info_list = [];
     var result = await conn.query(
-      'select name, email, age, sex, height, weight, dt, password, id from user where email = "$Email"');
+        'select name, email, age, sex, height, weight, dt, password, id from user where email = "$Email"');
     for (var row in result) {
       Session.instance.setInfo({
         'name': row[0],
-        'email' : row[1],
-        'age' : row[2],
-        'sex' : row[3],
-        'height' : row[4],
-        'weight' : row[5],
-        'dt' : row[6],
-        'password' : row[7],
-        'id' : row[8],
+        'email': row[1],
+        'age': row[2],
+        'sex': row[3],
+        'height': row[4],
+        'weight': row[5],
+        'dt': row[6],
+        'password': row[7],
+        'id': row[8],
       });
     }
     return print(Session.instance.userInfo);
@@ -113,149 +119,137 @@ class _loginPageState extends State<loginPage> {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: (MediaQuery.of(context).size.height) * 0.08,
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              constraints: const BoxConstraints(maxWidth: 300),
+              width: (MediaQuery.of(context).size.width) * 0.75,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height) * 0.075,
+                  ),
+                  // logo ----------------------------------------------------
+                  SizedBox(
+                      height: 100,
+                      child: Image.asset('assets/image/logo.png',
+                          fit: BoxFit.fitHeight)),
+                  // title ---------------------------------------------------
+                  Text(
+                    'AI DANG',
+                    textScaleFactor: 4,
+                    style: TextStyle(fontWeight: FontWeight.w600, color: red),
+                  ),
+                  // desc ----------------------------------------------------
+                  Text(
+                    '나만의 스마트한 당뇨관리 비서',
+                    textScaleFactor: 1.3,
+                    style: TextStyle(color: red),
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  SizedBox(
+                    child: TextField(
+                      controller: _idTextEditController,
+                      cursorColor: red,
+                      focusNode: _focusNodes[0],
+                      decoration: InputDecoration(
+                        labelText: '이메일 주소',
+                        hintText: '이메일 주소를 입력하세요.',
+                        labelStyle: TextStyle(color: darKGray),
+                        prefixIcon: Icon(Icons.email,
+                          color: _focusNodes[0].hasFocus ? red: darKGray
+                        ),
+                        focusColor: red,
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: gray),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: red),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: darKGray),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: _passwordTextEditController,
+                    cursorColor: red,
+                    focusNode: _focusNodes[1],
+                    decoration: InputDecoration(
+                      labelText: '비밀번호',
+                      hintText: '비밀번호를 입력하세요.',
+                      labelStyle: TextStyle(color: darKGray),
+                      prefixIcon: Icon(Icons.vpn_key,
+                          color: _focusNodes[1].hasFocus ? red: darKGray
+                      ),
+                      focusColor: red,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(width: 2, color: gray),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(width: 2, color: red),
+                      ),
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(color: darKGray),
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height) * 0.125,
+                  ),
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height) * 0.065,
+                    width: (MediaQuery.of(context).size.width) -
+                        (MediaQuery.of(context).size.width) * 0.40,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        user_check(_idTextEditController.text,
+                            _passwordTextEditController.text);
+                      },
+                      child: Text(
+                        '지금 시작하기',
+                        style: TextStyle(
+                            fontSize:
+                                ((MediaQuery.of(context).size.width) * 0.16) *
+                                    0.26),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        primary: red,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height) * 0.085,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => signup()),
+                        );
+                      },
+                      child: Text(
+                        '처음이신가요? 회원가입',
+                        style: TextStyle(
+                            fontSize:
+                                ((MediaQuery.of(context).size.width) * 0.16) *
+                                    0.26,
+                            color: red),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                color: Colors.white,
-                height: (MediaQuery.of(context).size.height) -
-                    (MediaQuery.of(context).size.height) * 0.18,
-                width: (MediaQuery.of(context).size.width) -
-                    (MediaQuery.of(context).size.width) * 0.3,
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'AI Dang',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: (MediaQuery.of(context).size.width) * 0.16,
-                          color: Color(0xffCF2525)),
-                    ),
-                    Text(
-                      '나만의 스마트한 당뇨관리 비서',
-                      style: TextStyle(
-                          fontSize:
-                              ((MediaQuery.of(context).size.width) * 0.16) *
-                                  0.26,
-                          color: Color(0xffCF2525)),
-                    ),
-                    SizedBox(
-                      height: (MediaQuery.of(context).size.height) * 0.20,
-                    ),
-                    SizedBox(
-                      width: (MediaQuery.of(context).size.width) -
-                          (MediaQuery.of(context).size.width) * 0.35,
-                      child: TextField(
-                        controller: _idTextEditController,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Enter your email',
-                          labelStyle: TextStyle(color: Color(0xffCF2525)),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                            borderSide:
-                                BorderSide(width: 1, color: Color(0xffCF2525)),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                            borderSide:
-                                BorderSide(width: 1, color: Color(0xffCF2525)),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                          ),
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                    ),
-                    SizedBox(
-                      height: (MediaQuery.of(context).size.height) * 0.01,
-                    ),
-                    SizedBox(
-                      width: (MediaQuery.of(context).size.width) -
-                          (MediaQuery.of(context).size.width) * 0.35,
-                      child: TextField(
-                        controller: _passwordTextEditController,
-                        decoration: const InputDecoration(
-                          // filled: true,
-                          labelText: 'Password',
-                          hintText: 'Enter your password',
-                          labelStyle: TextStyle(color: Color(0xffcf2525)),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                            borderSide:
-                                BorderSide(width: 1, color: Color(0xffCF2525)),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                            borderSide:
-                                BorderSide(width: 1, color: Color(0xffCF2525)),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                          ),
-                        ),
-                        obscureText: true,
-                      ),
-                    ),
-                    SizedBox(
-                      height: (MediaQuery.of(context).size.height) * 0.125,
-                    ),
-                    SizedBox(
-                      height: (MediaQuery.of(context).size.height) * 0.065,
-                      width: (MediaQuery.of(context).size.width) -
-                          (MediaQuery.of(context).size.width) * 0.40,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          user_check(_idTextEditController.text,
-                              _passwordTextEditController.text);
-                        },
-                        child: Text(
-                          '지금 시작하기',
-                          style: TextStyle(
-                              fontSize:
-                                  ((MediaQuery.of(context).size.width) * 0.16) *
-                                      0.26),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0)),
-                          primary: Color(0xffCF2525),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: (MediaQuery.of(context).size.height) * 0.085,
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => signup()),
-                          );
-                        },
-                        child: Text(
-                          '처음이신가요? 회원가입',
-                          style: TextStyle(
-                              fontSize:
-                                  ((MediaQuery.of(context).size.width) * 0.16) *
-                                      0.26,
-                              color: Color(0xffCF2525)),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
