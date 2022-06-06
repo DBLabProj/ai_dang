3d70e48ff6ba102d77e703bcf67c62bf3a0df7b1
yso009 <yso009@naver.com>
2022-06-06 17:22:46 +0900

    docs : move changePassword in setting dir

--- lib/views/setting/setting.dart
+++ lib/views/setting/setting.dart
@@ -1,4 +1,4 @@
-import 'package:ai_dang/views/account/changePassword.dart';
+import 'package:ai_dang/views/setting/changePassword.dart';
 import 'package:flutter/material.dart';
 
 import '../../session.dart';
