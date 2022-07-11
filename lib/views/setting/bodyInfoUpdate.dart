f0d2e5883028bcb00d7d5bee4cd7146c8b5163b1
yso009 <yso009@naver.com>
2022-07-11 20:25:25 +0900

    feat :

---
+++ lib/views/setting/bodyInfoUpdate.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class bodyInfoUpdate extends StatelessWidget {
+  const bodyInfoUpdate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
