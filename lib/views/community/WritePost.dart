397a65da306716460f07d21ee977548ed6878b05
Heebeom Yang <takeny1998@gmail.com>
2022-07-29 16:41:54 +0900

    fix: write post
    1. 글쓰기 위젯 별도 class로 분리
     - statless 위젯에서 stateful 위젯으로 전환
    2. 글쓰기에서 이미지 출력 시 이미지 이름이 안나오는 버그 수정
     - setstate() 처리

---
+++ lib/views/community/WritePost.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../dbHandler.dart';
+import '../../request.dart';
+import '../../session.dart';
+import 'community.dart';
+
+var colorBlack = const Color(0xff535353);
+var colorRed = const Color(0xffCF2525);
+var colorLightGray = const Color(0xffF3F3F3);
+var colorGray = const Color(0xffE0E0E0);
+var colorDarkGray = const Color(0xffADADBE);
+var colorOrange = const Color(0xffFBAA47);
+var colorGreen = const Color(0xff8AD03C);
+
+class WritePost extends StatefulWidget {
+
+  const WritePost({Key? key}) : super(key: key);
+
+  @override
+  State<WritePost> createState() => _WritePostState();
+}
+
+class _WritePostState extends State<WritePost> {
+  var writeBoardList = [];
+  var imageText;
+  final _picker = ImagePicker();
+
+  final _titleTextEditController = TextEditingController();
+  final _contentTextEditController = TextEditingController();
+
+  var user_id = Session.instance.userInfo['email'];
+  String _title = '';
+  String _content = '';
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    void showConfirmDialog(message) {
+      showDialog(
+        context: context,
+        barrierDismissible: true,
+        // false, //다이얼로그 바깥을 터치 시에 닫히도록 하는지 여부 (true: 닫힘, false: 닫히지않음)
+        builder: (BuildContext context) {
+          return
+            AlertDialog(
+              title: Text(
+                //제목 정의
+                message,
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // 현재 화면을 종료하고 이전 화면으로 돌아가기
+                    insertBoard(_title, _content, user_id);
+                  },
+                  child: const Text(
+                    '확인',
+                  ),
+                ),
+              ],
+            );
+        },
+      );
+    }
+
+    print(imageText);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '아이당 커뮤니티',
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: (MediaQuery.of(context).size.width)*0.04
+          ),
+        ),
+        backgroundColor: colorRed,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 2, 20, 0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              margin: EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: _titleTextEditController,
+                    onChanged: (text) {
+                      _title = text;
+                    },
+                    decoration: const InputDecoration(
+                      labelStyle: TextStyle(color: Color(0xffCF2525)),
+                      border: OutlineInputBorder(),
+                      labelText: "제목",
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                      ),
+                    ),
+                  ),
+                  ButtonTheme(
+                      minWidth: (MediaQuery.of(context).size.width),
+                      height: (MediaQuery.of(context).size.height) * 0.02,
+                      child: OutlinedButton(
+                        onPressed: () async {
+                          var imageName = await boardImage(context, ImageSource.gallery, _picker);
+                          print("------");
+                          print(imageName['img_name']);
+                          print("------");
+                          setState(() {
+                            imageText = imageName['img_name'];
+                          });
+                          print(imageText);
+                        },
+                        child: Text(
+                          '이미지 첨부하기',
+                          style: TextStyle(
+                              color: colorRed,
+                              fontWeight: FontWeight.w500,
+                              fontSize: (MediaQuery.of(context).size.width)*0.04
+                          ),
+                        ),
+                      )
+                  ),
+                  if(imageText != null)...[
+                    Text(imageText),
+                  ],
+                  TextField(
+                    controller: _contentTextEditController,
+                    onChanged: (text) {
+                      _content = text;
+                    },
+                    decoration: const InputDecoration(
+                      labelStyle: TextStyle(color: Color(0xffCF2525)),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 200.0, horizontal: 10),
+                      border: OutlineInputBorder(),
+                      labelText: "내용",
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      OutlinedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            '뒤 로 가 기',
+                            style: TextStyle(
+                                color: colorRed,
+                                fontWeight: FontWeight.w500,
+                                fontSize: (MediaQuery.of(context).size.width)*0.04
+                            ),
+                          )),
+                      ElevatedButton(
+                        onPressed: () {
+                          insertBoard(_title, _content, User_id);
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          '작 성 하 기',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: (MediaQuery.of(context).size.width)*0.04
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: colorRed,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/community/community.dart
+++ lib/views/community/community.dart
@@ -9,6 +9,7 @@ import 'package:ai_dang/dbHandler.dart';
 
 import '../../request.dart';
 import '../../session.dart';
+import 'WritePost.dart';
 import 'communityBuilder.dart';
 
 var colorBlack = const Color(0xff535353);
@@ -191,7 +192,7 @@ class _communityState extends State<community> {
         onPress: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => Write()),
+            MaterialPageRoute(builder: (context) => const WritePost()),
           );
         },
       );
@@ -221,173 +222,5 @@ class _communityState extends State<community> {
         ),
     );
     // return Scaffold
-  }
-}
-
-//글쓰기 위젯
-class Write extends StatelessWidget {
-  var writeBoardList = [];
-  var imageText;
-  final _picker = ImagePicker();
-
-  final _titleTextEditController = TextEditingController();
-  final _contentTextEditController = TextEditingController();
-
-  String _title = '';
-  String _content = '';
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    void showConfirmDialog(message) {
-      showDialog(
-        context: context,
-        barrierDismissible: true,
-        // false, //다이얼로그 바깥을 터치 시에 닫히도록 하는지 여부 (true: 닫힘, false: 닫히지않음)
-        builder: (BuildContext context) {
-          return
-            AlertDialog(
-              title: Text(
-                //제목 정의
-                message,
-              ),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); // 현재 화면을 종료하고 이전 화면으로 돌아가기
-                    insertBoard(_title, _content, User_id);
-                  },
-                  child: const Text(
-                    '확인',
-                  ),
-                ),
-              ],
-            );
-        },
-      );
-    }
-
-    print(imageText);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '아이당 커뮤니티',
-          style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w600,
-              fontSize: (MediaQuery.of(context).size.width)*0.04
-          ),
-        ),
-        backgroundColor: colorRed,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 2, 20, 0),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Container(
-              margin: EdgeInsets.all(8),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: _titleTextEditController,
-                    onChanged: (text) {
-                      _title = text;
-                    },
-                    decoration: const InputDecoration(
-                      labelStyle: TextStyle(color: Color(0xffCF2525)),
-                      border: OutlineInputBorder(),
-                      labelText: "제목",
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
-                      ),
-                    ),
-                  ),
-                  ButtonTheme(
-                    minWidth: (MediaQuery.of(context).size.width),
-                    height: (MediaQuery.of(context).size.height) * 0.02,
-                    child: OutlinedButton(
-                      onPressed: () async {
-                        var imageName = await boardImage(context, ImageSource.gallery, _picker);
-                        print("------");
-                        print(imageName['img_name']);
-                        print("------");
-                        imageText = imageName['img_name'];
-                        print(imageText);
-                      },
-                      child: Text(
-                        '이미지 첨부하기',
-                        style: TextStyle(
-                          color: colorRed,
-                          fontWeight: FontWeight.w500,
-                          fontSize: (MediaQuery.of(context).size.width)*0.04
-                        ),
-                      ),
-                      )
-                    ),
-                  if(imageText != null)...[
-                    Text(imageText),
-                  ],
-                  TextField(
-                    controller: _contentTextEditController,
-                    onChanged: (text) {
-                      _content = text;
-                    },
-                    decoration: const InputDecoration(
-                      labelStyle: TextStyle(color: Color(0xffCF2525)),
-                      contentPadding: const EdgeInsets.symmetric(vertical: 200.0, horizontal: 10),
-                      border: OutlineInputBorder(),
-                      labelText: "내용",
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(width: 1, color: Color(0xffCF2525)),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      OutlinedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text(
-                              '뒤 로 가 기',
-                              style: TextStyle(
-                                  color: colorRed,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: (MediaQuery.of(context).size.width)*0.04
-                              ),
-                          )),
-                      ElevatedButton(
-                          onPressed: () {
-                            insertBoard(_title, _content, User_id);
-                            Navigator.pop(context);
-                        },
-                          child: Text(
-                              '작 성 하 기',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: (MediaQuery.of(context).size.width)*0.04
-                              ),
-                          ),
-                        style: ElevatedButton.styleFrom(
-                          primary: colorRed,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
