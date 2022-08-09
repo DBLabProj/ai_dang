391eab2988351bb16efbd66a58c37192d2582786
yso009 <yso009@naver.com>
2022-08-09 21:53:31 +0900

    feat : alert, add bloodcheck data 1. 혈당 미입력시 alert 2. 혈당 정상 입력 후 alert 3. 혈당 입력 데이터, 메모 blood table 저장

--- lib/views/bloodCheck.dart
+++ lib/views/bloodCheck.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:ai_dang/utils/dbHandler.dart';
 import 'package:ai_dang/widgets/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,10 +7,9 @@ import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_picker/flutter_picker.dart';
-
+import 'package:ai_dang/utils/session.dart';
 import '../main.dart';
 import 'Statistics/statistics.dart';
-import 'loginPage.dart';
 
 class bloodCheck extends StatefulWidget {
   const bloodCheck({Key? key}) : super(key: key);
@@ -21,16 +20,21 @@ class bloodCheck extends StatefulWidget {
 
 class _bloodCheckState extends State<bloodCheck> {
 
-  DateTime _selectedDay = DateTime.now().toUtc().add(const Duration(hours: 9));
-  var _calendarFormat = CalendarFormat.week;
+  final DateTime _selectedDay = DateTime.now().toUtc().add(const Duration(hours: 9));
 
   var timeString ;
-  final controller = TextEditingController();
+  final bloodsugar_level = TextEditingController();
+  final memo_controller = TextEditingController();
+
+
+  var User_email = Session.instance.userInfo['email'];
+
 
 
   @override
   void dispose() {
-    controller.dispose();
+    bloodsugar_level.dispose();
+    memo_controller.dispose();
     super.dispose();
   }
 
@@ -71,6 +75,17 @@ class _bloodCheckState extends State<bloodCheck> {
           ),
           actions: <Widget>[
             TextButton(onPressed: (){
+              if(timeString == null ){
+                timeString = time[0];
+              }
+
+              if(bloodsugar_level.text == '') {
+                showDialogPop();
+              }else{
+                insertBloodCheck(User_email, timeString, bloodsugar_level.text, memo_controller.text );
+                showDialog_Done();
+              }
+
 
             },
                 child: Text(
@@ -92,11 +107,14 @@ class _bloodCheckState extends State<bloodCheck> {
                       thickness: 1,
                     ),
                     SizedBox(
-                      height: (MediaQuery.of(context).size.height) * 0.07,
+                      height: (MediaQuery.of(context).size.height) * 0.04,
                     ),
-                    insert_blood_data(),
-                    time_detail(),
-                    test_button(),
+                    insert_blood_data(User_email),
+                    SizedBox(
+                      height: (MediaQuery.of(context).size.height) * 0.04,
+                    ),
+                    memo_board(),
+
                   ],
                 ),
               ),
@@ -107,24 +125,21 @@ class _bloodCheckState extends State<bloodCheck> {
     );
   }
 
+  // ignore: non_constant_identifier_names
   Widget date_time(){
     return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 10, 10, 7),
-      child: Container(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 7),
+      child: SizedBox(
         height: 30,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('${_selectedDay}'.substring(0,4)+'년 '+'${_selectedDay}'.substring(5,7)+'월 '+'${_selectedDay}'.substring(8,10)+'일',
+            Text('$_selectedDay'.substring(0,4)+'년 '+'$_selectedDay'.substring(5,7)+'월 '+'$_selectedDay'.substring(8,10)+'일',
             textScaleFactor: 1.3,
-              style: TextStyle(
-            ),),
-
-
-            Text('${_selectedDay}'.substring(11,13)+'시 '+'${_selectedDay}'.substring(14,16)+'분 ',
+              ),
+            Text('$_selectedDay'.substring(11,13)+'시 '+'$_selectedDay'.substring(14,16)+'분 ',
             textScaleFactor: 1.3,
-              style: TextStyle(
-            ),),
+              ),
           ],
         ),
       ),
@@ -132,7 +147,8 @@ class _bloodCheckState extends State<bloodCheck> {
   }
 
 
-  Widget insert_blood_data(){
+  // ignore: non_constant_identifier_names
+  Widget insert_blood_data(userEmail){
     return Container(
       // color: Colors.blue,
       // width: (MediaQuery.of(context).size.width)*0.7,
@@ -163,6 +179,7 @@ class _bloodCheckState extends State<bloodCheck> {
     '기타',
   ];
 
+  // ignore: non_constant_identifier_names
   Widget picker_time() {
     return Container(
       width: (MediaQuery.of(context).size.width)*0.4,
@@ -198,6 +215,7 @@ class _bloodCheckState extends State<bloodCheck> {
 
 
 
+  // ignore: non_constant_identifier_names
   Widget Number_box() {
     return Container(
         width: (MediaQuery.of(context).size.width)*0.35,
@@ -207,18 +225,9 @@ class _bloodCheckState extends State<bloodCheck> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
 
-            // TextField(
-            //   onChanged: (test) {
-            //     print("첫 번째 텍스트 : $test");
-            //   },
-            // ),
-            // TextField(
-            //   controller: controller,
-            //
-            // )
 
             TextField(
-              controller: controller,
+              controller: bloodsugar_level,
               decoration: InputDecoration(
               hintText: '120',
               hintStyle: TextStyle(
@@ -227,7 +236,7 @@ class _bloodCheckState extends State<bloodCheck> {
                 color: Colors.grey[300]
               ),
 
-              contentPadding: EdgeInsets.only(left:30),
+              contentPadding: const EdgeInsets.only(left:30),
               border: InputBorder.none),
               style: TextStyle(
                 fontWeight: FontWeight.w700,
@@ -243,6 +252,7 @@ class _bloodCheckState extends State<bloodCheck> {
         ));
   }
 
+  // ignore: non_constant_identifier_names
   Widget mg_dl() {
     return Text(
       'mg/dL',
@@ -253,37 +263,103 @@ class _bloodCheckState extends State<bloodCheck> {
     );
   }
 
-  Widget time_detail() {
-    return Text(
-      '${controller.text}'
+  // ignore: non_constant_identifier_names
+  Widget memo_board() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: TextFormField(
+        controller: memo_controller,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          // hintText: '자세히 기록해 두세요!11',
+          labelText: ' 자세히 기록해 두세요!',
+
+          contentPadding: const EdgeInsets.symmetric(vertical: 50),
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(
+              color: Colors.blue
+            )
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(
+              color: colorRed
+            )
+          )
+        ),
+      ),
     );
   }
 
 
-  Widget test_button() {
-    double areaWidth = (MediaQuery.of(context).size.width) * 0.75;
-    if (areaWidth > 300) {
-      areaWidth = 300;
-    }
-    return SizedBox(
-      width: areaWidth * 0.75,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: () {
-          if(timeString == null ){
-            timeString = time[0];
-          }
-          print(timeString);
-          print(controller.text);
-        },
-        child:
-        const Text('지금 시작하기', textScaleFactor: 1.4),
-        style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0)),
-          primary: red,
-        ),
-      ),
+  void showDialogPop() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        // false, //다이얼로그 바깥을 터치 시에 닫히도록 하는지 여부 (true: 닫힘, false: 닫히지않음)
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            content: SingleChildScrollView(
+              child: Text.rich(
+                TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(text: '혈당', style: TextStyle(color: colorRed)),
+                    TextSpan(text: ' 수치가 입력되지 않았어요..')
+                  ]
+                )
+              )
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  '닫기',
+                  style: TextStyle(
+                      color: red
+                  ),
+                ),
+              )
+            ],
+          );
+        }
+    );
+  }
+
+  void showDialog_Done() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        // false, //다이얼로그 바깥을 터치 시에 닫히도록 하는지 여부 (true: 닫힘, false: 닫히지않음)
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            content: SingleChildScrollView(
+                child: Text(
+                  '정상적으로 기록되었습니다.'
+                )
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  '닫기',
+                  style: TextStyle(
+                      color: red
+                  ),
+                ),
+              )
+            ],
+          );
+        }
     );
   }
 
